98afa077e5a16f2758ec204ec5601262337d51e7
den8848 <denis2016wolverine2016@gmail.com>
2021-03-11 12:22:36 +0200

    feat: edit function towel sort

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@
 module.exports = function towelSort (matrix) {
 
   let newArr = []
-  if (matrix.length === 0) {
+  if (!matrix || matrix.length === 0) {
     return newArr
   }
   for (let i = 0; i < matrix.length; i++) {
     newArr = newArr.concat(matrix[i])
   }
-  // return newArr.sort()
-  return newArr
-
+  return newArr.sort(function(a, b) { return a - b })
 }
